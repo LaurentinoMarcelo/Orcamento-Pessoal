b5a778595ddedec3ca65b4b2fd1cc54660961f58
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2021-09-16 09:27:37 -0300

    Finalizado aplicação
    Finalizado a listagem de despesa, implementação de filtragem de despesa e remoção de despesa.

--- app.js
+++ app.js
@@ -50,10 +50,48 @@ class Bd{
             if(despesa === null){
                 continue;
             }
+            despesa.id = i;
             despesas.push(despesa);
         }
         return despesas;
     }
+
+    pesquisar(despesa){
+        let despesasFiltradas =  Array();
+
+        despesasFiltradas = this.recuperarTodosRegistros();
+
+        if(despesa.ano != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano);
+        }
+
+        if(despesa.mes != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes);
+        }
+
+        if(despesa.dia != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia);
+        }
+
+        if(despesa.tipo != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo);
+        }
+
+        if(despesa.descricao != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao);
+        }
+
+        if(despesa.descricao != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor);
+        }
+
+        return despesasFiltradas;
+    }
+
+    remover(id){
+        localStorage.removeItem(id);
+    }
+
 }
 
 let bd= new Bd()
@@ -87,6 +125,13 @@ function cadastrarDespesas() {
 
          $('#modalRegistroDespesa').modal('show')
 
+         ano.value = '';
+         mes.value = '';
+         dia.value = '';
+         tipo.value = '';
+         descricao.value = '';
+         valor.value = '';
+
     }else{
         document.getElementById('modal_titulo').innerHTML = "Erro na gravação da despesa";
         document.getElementById('modal_titulo_div').className = "modal-header text-danger";
@@ -104,22 +149,24 @@ function gravar(d){
 }
 
 
-function carregarListaDespesas(){
+function carregarListaDespesas(despesas = Array(), filtro = false){
 
-    let despesas = Array();
-
-    despesas = bd.recuperarTodosRegistros();
+    if (despesas.length == 0 && filtro == false) {
+        despesas = bd.recuperarTodosRegistros();
+    }
+   
 
     let listaDepesas = document.getElementById('listaDespesas');
+    listaDepesas.innerHTML = '';
 
-    /*<tr>
+            /*<tr>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
-              </tr>*/
+            </tr>*/
     despesas.forEach(function(d){
-        let linha = listaDespesas.insertRow();
+        let linha = listaDepesas.insertRow();
 
         linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
         
@@ -139,5 +186,40 @@ function carregarListaDespesas(){
         linha.insertCell(1).innerHTML = d.tipo;
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
+
+        let btn = document.createElement("button");
+        btn.className = 'btn btn-danger';
+        btn.innerHTML = '<i class="fas fa-times"></i>';
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            let id = this.id.replace('id_despesa_', '');
+            bd.remover(id);
+            window.location.reload();
+        }
+        linha.insertCell(4).append(btn);
     })
+
+
+}
+
+function pesquisarDespesa(){
+    let ano = document.getElementById('ano').value;
+    let mes = document.getElementById('mes').value;
+    let dia = document.getElementById('dia').value;
+    let tipo = document.getElementById('tipo').value;
+    let descricao = document.getElementById('descricao').value;
+    let valor = document.getElementById('valor').value;
+
+    let despesa = new Despesa(
+        ano,
+        mes,
+        dia,
+        tipo,
+        descricao,
+        valor
+    )
+
+    let despesas = bd.pesquisar(despesa);
+
+    this.carregarListaDespesas(despesas, true);
 }
